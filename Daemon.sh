8843f07419e261dc1206bd83a03873b8db62dd08
ggcs <41703231+ggcs@users.noreply.github.com>
2019-07-17 08:26:36 +0800

    Create Daemon.sh

---
+++ Daemon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python_ver=$(ls /usr/bin|grep -e "^python[2]\.[1-9]\+$"|tail -1)
+ps -ef | grep "[0-9] ${python_ver} server\\.py m"
+if [ $? -ne 0 ]
+then
+	cd `dirname $0`
+	ulimit -n 512000
+	nohup ${python_ver} server.py m>> /dev/null 2>&1 &
+fi
