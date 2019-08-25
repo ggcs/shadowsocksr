e81427dea0776ac56a00786660eb5118e68e70c1
ggcs <41703231+ggcs@users.noreply.github.com>
2019-08-26 02:47:31 +0800

    Update Shadowsocksr1Click.sh

--- Shadowsocksr1Click.sh
+++ Shadowsocksr1Click.sh
@@ -32,9 +32,9 @@ install_ssr(){
 	read -e -p "是否添加守护？[Y/n]" ssr_Daemon
 	if [[ ${ssr_Daemon} != [Nn] ]];then
 		if [ ! -d "//var/spool/cron/crontabs/" ];then
-			echo "*/1 * * * * /root/shadowsocksr/Daemon.sh" >> /var/spool/cron/root
+			echo "*/1 * * * * bash /root/shadowsocksr/Daemon.sh" >> /var/spool/cron/root
 		else
-			echo "*/1 * * * * /root/shadowsocksr/Daemon.sh" >> /var/spool/cron/crontabs/root
+			echo "*/1 * * * * bash /root/shadowsocksr/Daemon.sh" >> /var/spool/cron/crontabs/root
 		fi
 		echo 'ssr已被守护'
 	fi
