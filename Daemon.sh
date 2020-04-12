a95595723bf159d80ae735ab6c0316b7ff46899c
ggcs <41703231+ggcs@users.noreply.github.com>
2020-04-12 12:32:04 +0000

    Update Daemon.sh

--- Daemon.sh
+++ Daemon.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-python_ver=$(ls /usr/bin|grep -e "^python[2]\.[1-9]\+$"|tail -1)
+python_ver=$(ls /usr/bin|grep -e "^python[23]\.[1-9]\+$"|tail -1)
 ps -ef | grep "[0-9] ${python_ver} server\\.py m"
 if [ $? -ne 0 ]
 then
 	cd `dirname $0`
 	ulimit -n 512000
+	ulimit -u 512000
 	nohup ${python_ver} server.py m>> /dev/null 2>&1 &
 fi
